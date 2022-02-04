c95f133ab4dababf49f4bf6ee5ceb7f708ff5cea
NaiaraSuelen <na_suelen25@hotmail.com>
2022-02-04 16:17:17 -0300

    Initial commit

---
+++ src/br/com/exercicios/contaBanco/ContaBanco.java
@@ -0,0 +1,113 @@
+package br.com.exercicios.contaBanco;
+
+public class ContaBanco {
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    public ContaBanco(){
+        this.setSaldo(0);
+        this.setStatus(false);
+    }
+
+    public void estadoAtual(){
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("status: " + this.getStatus());
+    }
+    public ContaBanco(float saldo, boolean status) {
+        this.saldo = saldo;
+        this.status = status;
+    }
+
+    public void abrirConta(String t){
+        this.setTipo(t);
+        this.setStatus(true);
+
+        if(t == "CC") this.setSaldo(50);
+        else if(t == "CP") this.setSaldo(150);
+    }
+
+    public void fecharConta(){
+        if(this.getSaldo() > 0)
+            System.out.println("Conta não pode ser fechada, contém saldo");
+        else if(this.getSaldo() < 0)
+            System.out.println("Conta não pode ser fechada, contém débitos");
+        else{
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso");
+        }
+    }
+
+    public void depositar(float v){
+        if(this.getStatus()){
+            this.setSaldo(this.getSaldo() + v);
+            System.out.println("Depósito realizado com sucesso!");
+        }
+        else System.out.println("Conta inativa para depósito!");
+    }
+
+    public void sacar(float v){
+        if(this.getStatus()){
+            if (getSaldo() >= v){
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saldo realizado com sucesso");
+            }
+            else System.out.println("Saldo insuficiente");
+        }
+        else System.out.println("Conta inativa!");
+    }
+
+    public void pagarMensal(){
+        int v = 0;
+        if(this.getTipo() == "CC")v = 12;
+        else if(this.getTipo() == "CP")v = 20;
+
+        if (this.getStatus())this.setSaldo(getSaldo() - v);
+        else System.out.println("Conta inativa!");
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
---
+++ src/br/com/exercicios/contaBanco/teste.java
@@ -0,0 +1,19 @@
+package br.com.exercicios.contaBanco;
+
+public class teste {
+    public static void main(String[] args) {
+
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("João Paulo");
+        p1.abrirConta("CC");
+        p1.estadoAtual();
+        p1.sacar(50f);
+        p1.fecharConta();
+        p1.estadoAtual();
+        p1.pagarMensal();
+        p1.sacar(10);
+        p1.depositar(20);
+
+    }
+}
