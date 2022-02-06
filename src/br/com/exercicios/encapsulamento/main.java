c772cdafb8f25f566b8241762b466021e983b5fd
NaiaraSuelen <na_suelen25@hotmail.com>
2022-02-06 16:21:32 -0300

    exercício controle remoto

---
+++ src/br/com/exercicios/encapsulamento/main.java
@@ -0,0 +1,11 @@
+package br.com.exercicios.encapsulamento;
+
+public class main {
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+
+        controle.ligar();
+        controle.maisVolume();
+        controle.abrirMenu();
+    }
+}
